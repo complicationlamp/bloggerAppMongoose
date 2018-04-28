5d092d08759235475a509b67e350542a28477379
k <lookerkj@protonmail.com>
2018-04-28 11:46:39 -0700

    made a lot of changes to the resturant file

--- models.js
+++ models.js
@@ -2,24 +2,24 @@
 
 const mongoose = require('mongoose');
 
-// this is our schema to represent a restaurant
-const restaurantSchema = mongoose.Schema({
-  name: {type: String, required: true},
-  borough: {type: String, required: true},
-  cuisine: {type: String, required: true},
-  address: {
-    building: String,
-    // coord will be an array of string values
-    coord: [String],
-    street: String,
-    zipcode: String
-  },
-  // grades will be an array of objects
-  grades: [{
-    date: Date,
-    grade: String,
-    score: Number
-  }]
+// this is our schema to represent a post
+const postSchema = mongoose.Schema({
+  title: {type: String, required: true},
+  content: {type: String, required: true},
+  author: {type: String, required: true},
+  created: {type: String, requiered: true}
+    // building: String,
+    // // coord will be an array of string values
+    // coord: [String],
+    // street: String,
+    // zipcode: String
+  // },
+  // // grades will be an array of objects
+  // grades: [{
+  //   date: Date,
+  //   grade: String,
+  //   score: Number
+  // }]
 });
 
 // *virtuals* (http://mongoosejs.com/docs/guide.html#virtuals)
@@ -27,32 +27,32 @@ const restaurantSchema = mongoose.Schema({
 // properties that are stored in the database. Here we use it
 // to generate a human readable string based on the address object
 // we're storing in Mongo.
-restaurantSchema.virtual('addressString').get(function() {
-  return `${this.address.building} ${this.address.street}`.trim()});
+// postSchema.virtual('addressString').get(function() {
+//   return `${this.address.building} ${this.address.street}`.trim()});
 
-// this virtual grabs the most recent grade for a restaurant.
-restaurantSchema.virtual('grade').get(function() {
-  const gradeObj = this.grades.sort((a, b) => {return b.date - a.date})[0] || {};
-  return gradeObj.grade;
-});
+// // this virtual grabs the most recent grade for a post.
+// postSchema.virtual('grade').get(function() {
+//   const gradeObj = this.grades.sort((a, b) => {return b.date - a.date})[0] || {};
+//   return gradeObj.grade;
+// });
 
 // this is an *instance method* which will be available on all instances
 // of the model. This method will be used to return an object that only
 // exposes *some* of the fields we want from the underlying data
-restaurantSchema.methods.serialize = function() {
+postSchema.methods.serialize = function() {
 
   return {
     id: this._id,
-    name: this.name,
-    cuisine: this.cuisine,
-    borough: this.borough,
-    grade: this.grade,
-    address: this.addressString
+    title: this.title,
+    content: this.content,
+    author: this.author,
+    created: this.created,
+    // address: this.addressString
   };
 }
 
 // note that all instance methods and virtual properties on our
 // schema must be defined *before* we make the call to `.model`.
-const Restaurant = mongoose.model('Restaurant', restaurantSchema);
+const Post = mongoose.model('Post', postSchema);
 
-module.exports = {Restaurant};
+module.exports = {Post};
